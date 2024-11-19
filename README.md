78bb2157a5e0e1cc003ff445c9015fd256e0467b
Victor chromebook <175169337+itsvictoroyedeji@users.noreply.github.com>
2024-11-18 21:33:48 -0500

    Add html, js, and add README code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# item-lister
+# A Random Item Lister
+
+Use this to list items you need (or want to do).
+
+Main difference between this and your "notes" app on your phone: Once you complete the item on your list, you can easily delete them with a click of a button.
+
+You can also search your list of items as well.
+
+> Note: List does go away if you refresh the page. I'll fix that soon. So don't refresh after making your list :!
---
+++ main.js
@@ -0,0 +1,72 @@
+var addForm = document.getElementById("addForm");
+var items = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Add Submit Event listener to form
+addForm.addEventListener('submit', addItem);
+// Add Click Event listener to form
+items.addEventListener('click', deleteItem);
+// Add keyup Event listener to search form
+filter.addEventListener('keyup', filterItem);
+
+
+function addItem(e) {
+  // prevent form submission
+  e.preventDefault();
+  // Get textItem value
+  var textItem = document.querySelector('#addForm input[type="text"]').value;
+  if (textItem == '') return false;
+  // Add li element
+  var newListItem = document.createElement('li');
+  // Add li element classes
+  newListItem.classList = "list-group-item";
+
+  // Add text node and Append Text to List
+  newListItem.appendChild(document.createTextNode(textItem));
+
+  // Add new delete button
+  var deleteBtn = document.createElement('button');
+  // Add class to button
+  deleteBtn.classList = "btn btn-danger btn-sm float-end delete";
+  // Add text node and append to list Item
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append delete button to list item
+  newListItem.appendChild(deleteBtn);
+  // Append new list item to form
+  items.appendChild(newListItem);
+  
+  // remove input value after submitting
+  addForm[0].value = "";
+
+}
+
+function deleteItem(e) {
+  // Get element for delete button
+  if (e.target.classList.contains("delete")) {
+    // Get button's parent element and remove it from the "items" list
+    items.removeChild(e.target.parentElement);
+  }
+}
+
+function filterItem(e) {
+  // Get filter text value
+  var filterText = e.target.value;
+  // convert to lowercase
+  filterText.toLowerCase();
+  // Get list items
+  var listItems = document.getElementsByTagName('li');
+  // Convert List items into an array, and get each item (must be done on same line!)
+  Array.from(listItems).forEach((item) => {
+    // Get first child of each list item, which is the text. Then convert into string using .textContent
+    var searchedItem = item.firstChild.textContent;
+
+    // if filter text doesn't show in item list, it returns -1. Lowercase the searched list as well for a match.
+    if (searchedItem.toLowerCase().indexOf(filterText) == -1) {
+      item.style.display = "none";
+    } else {
+      item.style.display = "block";
+    }
+  })
+}
+
